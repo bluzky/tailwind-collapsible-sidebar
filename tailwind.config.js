adf007159f3ff394e662fcd89df49d1da16d0aa2
CodeSandbox <support@codesandbox.io>
2023-05-03 07:47:15 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // uncomment this to enable Just-in-Time mode
+  // https://tailwindcss.com/docs/just-in-time-mode#enabling-jit-mode
+  // mode: "jit",
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  content: ["./public/**/*.html"],
+};
